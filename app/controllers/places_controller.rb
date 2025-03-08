a3c1049034e47b9c353afdb53db2c2927fe704cc
jdobie11 dobes <jonathon.dobie@kellogg.northwestern.edu>
2025-03-08 18:32:53 +0000

    CSS and routes

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,5 @@
 class PlacesController < ApplicationController
+  before_action :require_login, only: [:index, :new, :create]
 
   def index
     @places = Place.all
@@ -13,10 +14,23 @@ class PlacesController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "id" => session["user_id"] })
     @place = Place.new
     @place["name"] = params["name"]
-    @place.save
-    redirect_to "/places"
+    @place["user_id"] = @user["id"]
+    if @place.save
+      redirect_to "/places"
+    else
+      render :new
+    end
   end
 
+  private
+
+  def require_login
+    unless User.find_by({ "id" => session["user_id"] })
+      flash["notice"] = "Login first."
+      redirect_to "/login"
+    end
+  end
 end
